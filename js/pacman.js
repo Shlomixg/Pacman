6ffff6e281a65bb5e0959263ecd9fb2e8e5f6a06
Shlomi <Shlomixg@gmail.com>
2018-08-22 18:09:32 +0300

    tweaks

--- js/pacman.js
+++ js/pacman.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gPacman;
 var PACMAN = ` 
 <div class="pacman">
@@ -8,102 +10,95 @@ var PACMAN = `
 var gSuperTimeout;
 
 function createPacman(board) {
-  gPacman = {
-    location: {
-      i: 3,
-      j: 5
-    },
-    isSuper: false
-  };
-  board[gPacman.location.i][gPacman.location.j] = PACMAN;
+	gPacman = {
+		location: { i: 3, j: 5 },
+		isSuper: false
+	};
+	board[gPacman.location.i][gPacman.location.j] = PACMAN;
 }
 
 function movePacman(eventKeyboard) {
-  // console.log('eventKeyboard:', eventKeyboard);
+	// console.log('eventKeyboard:', eventKeyboard);
 
-  if (gState.isGameDone) return;
+	if (gState.isGameDone) return;
 
-  var nextLocation = {
-    i: gPacman.location.i,
-    j: gPacman.location.j
-  };
+	var nextLocation = {
+		i: gPacman.location.i,
+		j: gPacman.location.j
+	};
 
-  switch (eventKeyboard.code) {
+	switch (eventKeyboard.code) {
+		case 'ArrowUp':
+			var direction = 'up';
+			nextLocation.i--;
+			break;
+		case 'ArrowDown':
+			var direction = 'down';
+			nextLocation.i++;
+			break;
+		case 'ArrowLeft':
+			var direction = 'left';
+			nextLocation.j--;
+			break;
+		case 'ArrowRight':
+			var direction = 'right';
+			nextLocation.j++;
+			break;
+	}
 
-    case 'ArrowUp':
-      var direction = 'up';
-      nextLocation.i--;
-      break;
-    case 'ArrowDown':
-      var direction = 'down';
-      nextLocation.i++;
-      break;
-    case 'ArrowLeft':
-      var direction = 'left';
-      nextLocation.j--;
-      break;
-    case 'ArrowRight':
-      var direction = 'right';
-      nextLocation.j++;
-      break;
+	var nextCell = gBoard[nextLocation.i][nextLocation.j];
 
-  }
+	// hitting a wall, not moving anywhere
+	if (nextCell === WALL) return;
+	else if (nextCell === FOOD) eatFood();
+	else if (nextCell === POWER_FOOD) eatPowerFood();
+	else if (nextCell === CHERRY) updateScore(20);
 
-  var nextCell = gBoard[nextLocation.i][nextLocation.j];
-  // console.log('Heading: row:', newLocation.i , ' col: ', newLocation.j );
-  // console.log('Whats there:', gBoard[newLocation.i][newLocation.j]);
+	// Check if the player won
+	if (gState.foodies === 0 && gState.powerFoodies === 0) stopGame(true);
 
-  // hitting a wall, not moving anywhere
-  if (nextCell === WALL) return;
-  else if (nextCell === FOOD) eatFood();
-  else if (nextCell === POWER_FOOD) eatPowerFood();
-  else if (nextCell === CHERRY) updateScore(20);
+	var isGameOver = checkEngage(nextCell, GHOST, nextLocation.i, nextLocation.j);
+	if (isGameOver) return;
 
-  // Check if the player won
-  if (gState.foodies === 0 && gState.powerFoodies === 0) stopGame(true);
+	// update the model to reflect movement
+	gBoard[gPacman.location.i][gPacman.location.j] = EMPTY;
 
-  var isGameOver = checkEngage(nextCell, GHOST, nextLocation.i, nextLocation.j);
-  if (isGameOver) return;
+	// render updated model to the DOM
+	renderCell(gPacman.location, EMPTY);
 
-  // update the model to reflect movement
-  gBoard[gPacman.location.i][gPacman.location.j] = EMPTY;
+	// Update the pacman MODEL to new location  
+	gPacman.location = nextLocation;
+	gBoard[gPacman.location.i][gPacman.location.j] = PACMAN;
 
-  // render updated model to the DOM
-  renderCell(gPacman.location, EMPTY);
-
-  // Update the pacman MODEL to new location  
-  gPacman.location = nextLocation;
-  gBoard[gPacman.location.i][gPacman.location.j] = PACMAN;
-
-  // render updated model to the DOM
-  renderCell(gPacman.location, PACMAN);
-  var pac = document.querySelector('.pacman');
-  pac.classList.add(direction);
+	// render updated model to the DOM
+	renderCell(gPacman.location, PACMAN);
+	var pac = document.querySelector('.pacman');
+	pac.classList.add(direction);
 }
 
 function eatFood() {
-  updateScore(1);
-  gState.foodies--;
+	updateScore(1);
+	gState.foodies--;
 }
 
 function eatPowerFood() {
-  updateScore(15);
-  gState.powerFoodies--;
-  // TODO: if it's already super
-  if (gPacman.isSuper) {
-    clearTimeout(gSuperTimeout);
-  } else {
-    gPacman.isSuper = true;
-    gGhosts.forEach(function paintGhost(ghost) {
-      if (!ghost.isDead) renderCell(ghost.location, getGhostHTML(ghost));
-    });
-  }
+	updateScore(15);
+	gState.powerFoodies--;
+	// TODO: if it's already super
+	if (gPacman.isSuper) {
+		clearTimeout(gSuperTimeout);
+	} else {
+		gPacman.isSuper = true;
+		gGhosts.forEach(function paintGhost(ghost) {
+			if (!ghost.isDead) renderCell(ghost.location, getGhostHTML(ghost));
+		});
+	}
 
-  gSuperTimeout = setTimeout(function unsetSuper() {
-    gPacman.isSuper = false;
-    gGhosts.forEach(function ghostResuscitation(ghost) {
-      ghost.isDead = false;
-      renderCell(ghost.location, getGhostHTML(ghost));
-    });
-  }, 5000);
+	gSuperTimeout = setTimeout(function unsetSuper() {
+		gPacman.isSuper = false;
+		gGhosts.forEach(function ghostResuscitation(ghost) {
+			ghost.isDead = false;
+			renderCell(ghost.location, getGhostHTML(ghost));
+		});
+	}, 5000);
 }
